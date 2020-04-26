a04601b1a2fafbbdb78db45700c40bdad46259fe
Bryan Azofeifa <bazofeifa@gmail.com>
2020-04-25 19:43:47 -0600

    Update package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 import evaluate from './evaluate';
 
-const result: number = evaluate('nueve millones ciento cincuenta y cinco mil setecientos sesenta y ocho colones con setenta y dos céntimos');
-
-console.log('value', result);
+export default evaluate;
